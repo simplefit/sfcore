348fa9a6bee1acf4659b61e636a022fe9c921592
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-10-08 02:50:01 +0200

    Basic function base class

---
+++ function.cxx
@@ -0,0 +1,23 @@
+#include <stdexcept>
+#include <string>
+
+#include "function.hxx"
+
+
+function::operator double () const
+{
+  return _fptr(_num, _args);
+}
+
+void function::set_param(unsigned idx, double val)
+{
+  if (idx >= _num)
+    throw std::out_of_range("Index out of range, > than "
+			    + std::to_string(_num));
+  _args[idx] = val;
+}
+
+std::pair<int, double *> function::params() const
+{
+  return std::pair<int, double *> (_num, _args);
+}
---
+++ function.hxx
@@ -0,0 +1,26 @@
+#ifndef __simplefit_function__
+#define __simplefit_function__
+
+#include <utility>
+#include <stdexcept>
+#include <string>
+
+
+class function {
+public:
+  function(unsigned num, double * args, double (* fptr) (unsigned, double *))
+    : _num(num), _args(args), _fptr(fptr) {}
+  ~function() {};
+
+  operator double () const;
+
+  void set_param(unsigned idx, double val);
+  std::pair<int, double *> params() const;
+
+private:
+  unsigned _num;
+  double * _args;
+  double (* _fptr) (unsigned, double *);
+};
+
+#endif	// __simplefit_function__
