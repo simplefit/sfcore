5ab8b519f6727f38aa47dd8c4edbb8ccc4b77ba4
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-10-08 09:31:21 +0200

    Fixes 35a7160 (needs C++11)
    Need to enable C++11 to use std::to_string()

--- function.cxx
+++ function.cxx
@@ -12,9 +12,8 @@ function::operator double () const
 void function::set_param(unsigned idx, double val)
 {
   if (idx >= _num)
-    throw std::out_of_range("Index out of range, > than \n"
-			    // + std::to_string(_num)
-			    );
+    throw std::out_of_range("Index out of range, " + std::to_string(idx)
+			    + " >= " + std::to_string(_num) + "\n");
   _args[idx] = val;
 }
 
