9dec22bbe92f022dd8df580b326afb9ec95b678d
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-10-17 18:46:24 +0200

    Add variable tests

---
+++ tests/test_variable.cc
@@ -0,0 +1,41 @@
+#define BOOST_TEST_MODULE test_variable
+
+#include <stdexcept>
+
+#include <boost/test/included/unit_test.hpp>
+
+#include "variable.hxx"
+
+
+struct varsetup {
+  varsetup() : myvar(0, 5, 3) {}
+  ~varsetup() {}
+  variable myvar;
+};
+
+BOOST_AUTO_TEST_SUITE(test_variable)
+
+BOOST_FIXTURE_TEST_CASE(automatic_conversion, varsetup)
+{
+  BOOST_CHECK(3.0 == myvar);
+}
+
+BOOST_FIXTURE_TEST_CASE(assignment_operator, varsetup)
+{
+  variable myvar2 = myvar;
+  BOOST_CHECK(3 == myvar2);
+}
+
+BOOST_FIXTURE_TEST_CASE(setter_access, varsetup)
+{
+  myvar.set_val(1);
+  BOOST_CHECK(1 == myvar);
+}
+
+BOOST_FIXTURE_TEST_CASE(value_out_of_range_exception, varsetup)
+{
+  BOOST_CHECK_THROW(myvar.set_val(-1), std::out_of_range);
+  // BOOST_CHECK_EQUAL(myvar.set_val(-1), std::out_of_range, predicate);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
