c6c744373ed3fd6b63155d4de5233ea0fd32c57c
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-10-17 18:45:50 +0200

    Move to Boost.Test for unit tests

--- tests/test_function.cc
+++ tests/test_function.cc
@@ -1,9 +1,12 @@
-#include <iostream>
+#define BOOST_TEST_MODULE test_function
+
 #include <stdexcept>
+#include <boost/test/included/unit_test.hpp>
 
 #include "function.hxx"
 #include "variable.hxx"
 #include "coreutils.hxx"
+
 
 double sum(std::vector<double> & args)
 {
@@ -14,28 +17,45 @@ double sum(std::vector<double> & args)
   return res;
 }
 
-int main()
+struct fnsetup {
+  fnsetup() : myfunc(NULL)
+  {
+    double array[3] = {1, 2, 3};
+    myfunc = &make_function(sum, 3, array);
+  }
+  ~fnsetup() {}
+  function * myfunc;
+};
+
+BOOST_AUTO_TEST_SUITE(test)
+
+BOOST_FIXTURE_TEST_CASE(automatic_conversion, fnsetup)
 {
-  double array[3] = {1, 2, 3};
+  BOOST_CHECK_EQUAL(6.0, *myfunc);
+}
 
-  function &myfunc = make_function(sum, 3, array);
-
-  std::cout << "automatic conversion: myfunc: 1+2+3 = " << myfunc << std::endl;
-
-  auto& vars = myfunc.components();
+BOOST_FIXTURE_TEST_CASE(direct_access, fnsetup)
+{
+  auto& vars = myfunc->components();
   vars[1] = fnbase_ptr(new variable(3));
   vars[2] = fnbase_ptr(new variable(5));
-  std::cout << "parameter change (direct): myfunc: 1+3+5 = " << myfunc << std::endl;
-
-  try {
-    for (unsigned i = 0; i < 3; ++i) {
-      myfunc.replace(i, new variable((i+1) * 2));
-    }
-    std::cout << "parameter change (setter): myfunc: 2+4+6 = " << myfunc << std::endl;
-    myfunc.replace(3, new variable(2));
-  } catch (std::out_of_range &exc) {
-    std::cout << "exception handling: "<< exc.what() << std::endl;
-  }
-
-  return 0;
+  BOOST_CHECK_EQUAL(9, *myfunc);
 }
+
+BOOST_FIXTURE_TEST_CASE(setter_access, fnsetup)
+{
+  for (unsigned i = 0; i < 3; ++i) {
+    myfunc->replace(i, new variable((i+1) * 2));
+  }
+  BOOST_CHECK_EQUAL(12, *myfunc);
+}
+
+BOOST_FIXTURE_TEST_CASE(index_out_of_range_exception, fnsetup)
+{
+  BOOST_CHECK_THROW(myfunc->replace(3, new variable(2)),
+		    std::out_of_range);
+  // BOOST_CHECK_EXCEPTION(myfunc->replace(3, new variable(2)),
+  // 			std::out_of_range, predicate);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
