5359f2f66aeda2597c40bf4392486ba66b92f612
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-10-18 00:20:33 +0200

    Function assignment operator test, and minor fixes

--- tests/test_function.cc
+++ tests/test_function.cc
@@ -31,7 +31,7 @@ BOOST_AUTO_TEST_SUITE(test)
 
 BOOST_FIXTURE_TEST_CASE(automatic_conversion, fnsetup)
 {
-  BOOST_CHECK_EQUAL(6.0, *myfunc);
+  BOOST_CHECK_EQUAL(6.0, *myfunc); // 1+2+3
 }
 
 BOOST_FIXTURE_TEST_CASE(direct_access, fnsetup)
@@ -39,7 +39,7 @@ BOOST_FIXTURE_TEST_CASE(direct_access, fnsetup)
   auto& vars = myfunc->components();
   vars[1] = fnbase_ptr(new variable(3));
   vars[2] = fnbase_ptr(new variable(5));
-  BOOST_CHECK_EQUAL(9, *myfunc);
+  BOOST_CHECK_EQUAL(9, *myfunc); // 1+3+5
 }
 
 BOOST_FIXTURE_TEST_CASE(setter_access, fnsetup)
@@ -47,7 +47,13 @@ BOOST_FIXTURE_TEST_CASE(setter_access, fnsetup)
   for (unsigned i = 0; i < 3; ++i) {
     myfunc->replace(i, new variable((i+1) * 2));
   }
-  BOOST_CHECK_EQUAL(12, *myfunc);
+  BOOST_CHECK_EQUAL(12, *myfunc); // 2+4+6
+}
+
+BOOST_FIXTURE_TEST_CASE(assignment_operator, fnsetup)
+{
+  function newfn = *myfunc;
+  BOOST_CHECK_EQUAL(*myfunc, newfn);
 }
 
 BOOST_FIXTURE_TEST_CASE(index_out_of_range_exception, fnsetup)
--- tests/test_variable.cc
+++ tests/test_variable.cc
@@ -16,19 +16,19 @@ BOOST_AUTO_TEST_SUITE(test_variable)
 
 BOOST_FIXTURE_TEST_CASE(automatic_conversion, varsetup)
 {
-  BOOST_CHECK(3.0 == myvar);
-}
-
-BOOST_FIXTURE_TEST_CASE(assignment_operator, varsetup)
-{
-  variable myvar2 = myvar;
-  BOOST_CHECK(3 == myvar2);
+  BOOST_CHECK_EQUAL(3.0, myvar);
 }
 
 BOOST_FIXTURE_TEST_CASE(setter_access, varsetup)
 {
   myvar.set_val(1);
-  BOOST_CHECK(1 == myvar);
+  BOOST_CHECK_EQUAL(1, myvar);
+}
+
+BOOST_FIXTURE_TEST_CASE(assignment_operator, varsetup)
+{
+  variable myvar2 = myvar;
+  BOOST_CHECK_EQUAL(myvar, myvar2);
 }
 
 BOOST_FIXTURE_TEST_CASE(value_out_of_range_exception, varsetup)
