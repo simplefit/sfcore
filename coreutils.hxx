e346668d849b1d77cbe0951052e610281b7a4ac3
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-11-13 03:07:39 +0100

    Helper utils now supports all pod types

--- coreutils.hxx
+++ coreutils.hxx
@@ -2,12 +2,27 @@
 #define __simplefit_coreutils__
 
 #include <vector>
+#include <memory>
 
+#include "variable.hxx"
 #include "function.hxx"
 
 
-fnbase_ptr_vec pod_to_variable(std::vector<double> args);
+template <typename T>
+fnbase_ptr_vec pod_to_variable(std::vector<T> args)
+{
+  fnbase_ptr_vec vars(args.size());
+  for (unsigned i = 0; i < args.size(); ++i) {
+    vars[i] = std::make_shared<variable>(args[i]);
+  }
+  return vars;
+}
 
-fnbase_ptr_vec pod_to_variable(unsigned num, double * args);
+template <typename T>
+fnbase_ptr_vec pod_to_variable(unsigned num, T * args)
+{
+  std::vector<T> vals(args, args + num);
+  return pod_to_variable(vals);
+}
 
 #endif	// __simplefit_coreutils__
