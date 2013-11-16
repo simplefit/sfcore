94385fab0ca74355482d4b1a9104af20375f0ed9
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-11-16 19:41:47 +0100

    Add more operator tests

--- tests/test_operators.cc
+++ tests/test_operators.cc
@@ -30,6 +30,22 @@ struct fnsetup {
   function * myfunc2;
 };
 
+struct mix_fnsetup {
+  mix_fnsetup() : myvar(nullptr), myfunc(nullptr)
+  {
+    myvar = new variable(0, 10, 7);
+    double array[3] = {1, 2, 3};
+    myfunc = new function(sum, 3, array);
+  }
+  ~mix_fnsetup()
+  {
+    delete myvar;
+    delete myfunc;
+  }
+  variable * myvar;
+  function * myfunc;
+};
+
 BOOST_FIXTURE_TEST_CASE(binary, fnsetup)
 {
   function mysum = *myfunc1 + *myfunc2;
@@ -50,9 +66,34 @@ BOOST_FIXTURE_TEST_CASE(binary, fnsetup)
   BOOST_CHECK_EQUAL(5.0/2.0, mydiv2); // (4+5+6) / (1+2+3)
 
   function zero = *myfunc1 - *myfunc1;
-  BOOST_REQUIRE_EQUAL(0.0, zero);
-  function nan = *myfunc2 / zero;
-  BOOST_CHECK(std::isinf(nan));
+  BOOST_CHECK_EQUAL(0.0, zero);
+  function inf = *myfunc2 / zero;
+  BOOST_CHECK(std::isinf(inf));
+}
+
+BOOST_FIXTURE_TEST_CASE(mix_binary, mix_fnsetup)
+{
+  function mysum = *myvar + *myfunc;
+  BOOST_CHECK_EQUAL(13.0, mysum); // 7 + (1+2+3)
+
+  function mydiff_pve = *myvar - *myfunc;
+  BOOST_CHECK_EQUAL(1.0, mydiff_pve); // 7 - (1+2+3)
+  function mydiff_nve = *myfunc - *myvar;
+  BOOST_CHECK_EQUAL(-1.0, mydiff_nve); // (1+2+3) - 7
+
+  function myprod = *myvar * *myfunc;
+  BOOST_CHECK_EQUAL(42.0, myprod); // 7 * (1+2+3)
+
+  function mydiv1 = *myvar / *myfunc;
+  BOOST_CHECK_EQUAL(7.0/6.0, mydiv1); // 7 / (1+2+3)
+
+  function mydiv2 = *myfunc / *myvar;
+  BOOST_CHECK_EQUAL(6.0/7.0, mydiv2); // (1+2+3) / 7
+
+  function zero = *myfunc - *myfunc;
+  BOOST_CHECK_EQUAL(0.0, zero);
+  function inf = *myvar / zero;
+  BOOST_CHECK(std::isinf(inf));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
